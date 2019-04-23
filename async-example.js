2a1e39329863741dc1f1ed74c174566fe1dc230a
Sebastian <sebgertz@gmail.com>
2019-04-23 13:08:31 -0400

    stub out application flow

--- async-example.js
+++ async-example.js
@@ -5,3 +5,20 @@ const async   = require('async');
 const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather?q='
 const WEATHER_API_KEY = '88bfde3533d98e8a300a138ef668cda2';
 
+async.waterfall([
+  (callback) => {
+    prompt.get({
+      name: 'city',
+      description: 'Enter city to fetch its current weather'
+    }, (err, result) => {
+      if (err) return callback(err);
+      callback(null, result.city);
+    });
+  },
+  (city, callback) => {
+    console.log(`The user entered: ${city}`);
+    callback();
+  }
+], (err, results) => {
+  console.log("Here we'll output the result.");
+});
